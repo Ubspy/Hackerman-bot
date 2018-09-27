32148ef0f0b85fea93236c54db833fda6a379e7b
drhacko <dylhack@gmail.com>
2018-09-27 18:46:28 +0000

    little clean2

--- Bot.js
+++ Bot.js
@@ -4,59 +4,73 @@ const client = new Discord.Client();
 
 //Constant for commands file
 const {commands} = require('./src/');
-
-var commandChannels = [];
+const {prefix, token} = require('./config/Bot.json')
+const blacklist = [];
 
 //On ready, output logged in message
-client.on('ready', () =>
-{
-  console.log(`Logged in as ${client.user.tag}`);
-
-  //Gets command channels where you put bot commands in
-  commandChannels = Commands.getCommandChannels(client);
+client.once('ready', () => {
+	console.log("Logged in as "+client.user.username+"#"+client.user.discriminator);
+	console.log("Guild Count: "+client.guilds.array().length)
 });
 
+// Check if the bot is not talking to itself and the message's channel is not blacklisted
+function verify(message) {
+	if (message.content != undefined) {
+		if (!blacklist.includes(message.channel.id) && message.author.id != client.user.id) {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
+}
+// Authorization, currently only guild owner. (next should be bot owner see https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=fetchApplication)
+function authorize(member) {
+	if (member instanceof Discord.GuildMember) {
+		if (member.id == member.guild.ownerID) {
+			return true
+		} else {
+			return false
+		}
+	} else {
+		return false
+	}
+}
 //On a message, run this function
-client.on('message', message =>
-{
-  //If the message sent isn't sent by the bot
-  var correctAuthor = message.author != client.user;
-
-  //If the message is in a bot command channel
-  var correctChannel = commandChannels.indexOf(message.channel.id) > -1;
-
-  //String to hold message content
-  var messageStr = message.toString();
-
-  //If both of those cases meet
-  if(correctAuthor && correctChannel)
-  {
-    //If the first character is '!' implying it's a command
-    if(messageStr[0] == '!')
-    {
-      //Check for a space to just get the raw command
-      var endingIndex = (messageStr.indexOf(' ') > -1) ? messageStr.indexOf(' ') : messageStr.length;
-
-      //String to hold commands
-      var command = messageStr.substring(1, endingIndex);
-
-      //If it's the clear command, run the clear command funciton
-      if(command == "clear")
-      {
-        Commands.clearChannel(message);
-      }
-    }
-  }
-
-  //Just if correct author
-  if(correctAuthor)
-  {
-    if(messageStr.indexOf('r/') > -1)
-    {
-      Commands.linkSubreddit(message);
-    }
-  }
-});
+client.on('message', msg => {
+	if(authorize(msg.member)) {
+		if (msg.content == prefix+"blacklist") {
+			blacklist.push(msg.channel.id)
+		}
+		if (msg.content == prefix+"unblacklist") {
+			for (var i = 0; i < blacklist.length; i++) {
+				if (blacklist[i] != undefined && blacklist[i] == msg.channel.id) {
+					delete blacklist[i]
+					msg.reply("Channel unblacklisted")
+				} else {
+					msg.reply("Channel is probably not blacklisted.")
+				}
+			} 
+		}
+	}
+	// If the channel is not a blacklist, and is a TextChannel
+	if(verify(msg)) {	
+      		//Check for a space to just get the raw command
+      		var endingIndex = (msg.content.indexOf(' ') > -1) ? msg.content.indexOf(' ') : msg.content.length;
+      		//String to hold commands
+      		var command = msg.content.substring(prefix.length, endingIndex);
+		// If it's the clear command, run the clear command funciton
+		if(command == prefix+"clear") {
+			commands.bulkDel(msg.channel)	
+			msg.reply("cleared the chat") 
+		}
+		if(msg.content.indexOf('r/') > -1) {
+			msg.channel.send("OwO, what's this? I see a mention of a subreddit! Here's a link: "+commands.subReddit(msg));
+		}
+	}
+	
+  });
 
 //Login command with bot token
-client.login();
+client.login(token);
--- src/commands.js
+++ src/commands.js
@@ -1,59 +1,29 @@
 const sleep = require('sleep');
-
+const {TextChannel, GuildChanne} = require('discord.js')
 //Clears the chat logs
-function clearMessages(message)
-{
-  //Gets all messages in the bot-commands channel
-  message.channel.fetchMessages().then(function(messages)
-  {
-    //Bulk deletes all the messages
-    message.channel.bulkDelete(messages);
-  });
-
-  //Sends a message
-  message.channel.send("Chat log has been cleared!").then(function(thisMessage)
-  {
-    //After the message it sent, it waits three seconds and deletes itself
-    sleep.sleep(3);
-    thisMessage.delete();
-  });
+function bulkDelete(textchan) {
+	if (textchan instanceof TextChannel && textchan.guild.me.hasPermission("MANAGE_MESSAGES") || textchan.memberPermissions(textchan.guild.me).has("MANAGE_MESSAGES")) {
+		return true
+		//Gets all messages in the bot-commands channel
+		textchan.fetchMessages().then(function(messages) {
+		//Bulk deletes all the messages
+		textchan.bulkDelete(messages);
+	});
+	} else {
+		return null
+	}
+}
+function subReddit(message) {
+	//Gets starting index of the subreddit
+	var subCutoff = message.content.substring(message.content.indexOf('r/'), message.content.length);
+	//Check for a space to just get the raw subreddit
+	var endingIndex = (subCutoff.indexOf(' ') > -1) ? subCutoff.indexOf(' ') : subCutoff.length;
+	//Substring for the actual subreddit
+	var subredditStr = subCutoff.substring(0, endingIndex);
+	return "http://reddit.com/"+subredditStr
 }
 
-function linkSubreddit(message) {
-  //String of the message
-  var messageStr = message.toString();
 
-  //Gets starting index of the subreddit
-  var subCutoff = messageStr.substring(messageStr.indexOf('r/'), messageStr.length);
-
-  //Check for a space to just get the raw subreddit
-  var endingIndex = (subCutoff.indexOf(' ') > -1) ? subCutoff.indexOf(' ') : subCutoff.length;
-
-  //Substring for the actual subreddit
-  var subredditStr = subCutoff.substring(0, endingIndex);
-
-  message.channel.send("OwO, what's this? I see a mention of a subreddit! Here's a link: http://reddit.com/"+subredditStr, {reply: message.author});
-}
-
-function getCommandChannels(client) 
-{
-  //Variable of all channels the bot is connected to
-  var activeChannels = client.channels.array();
-
-  //Variable for command command channels
-  var commandChannels = [];
-
-  //Loops through all channels
-  for(var i = 0; i < activeChannels.length; i++)
-  {
-    if(activeChannels[i].name == "bot-commands")
-    {
-      commandChannels.push(activeChannels[i].id);
-    }
-  }
-
-  return commandChannels;
-}
-module.exports.commandChannels = getCommandChannels
-module.exports.linkReddit = linkSubreddit
-module.exports.clear = clearMessages
+module.exports.subReddit = subReddit
+module.exports.bulkDel = bulkDelete
+console.log("commands.js loaded")
