725c77945ddee008df6a6b6a9506691b16179be8
devr2k <dylhack@gmail.com>
2018-10-25 20:44:40 -0500

    SCP Lookup command added.

--- Bot.js
+++ Bot.js
@@ -6,9 +6,10 @@
  * @description
  * * Fill out config/Bot.json
  * * Commands:
- * * * clear 									`Attempts to delete all messages in the channel.`
  * * * blacklist/unblacklist 	`ignore the commands in the channel "blacklist" was delcared`
+ * * * clear 									`Attempts to delete all messages in the channel.`
  * * * r/unixporn							`The bot listens for any shorthand sub reddit links and automatically sends a hyperlink to it.`
+ * * * scp SCPID							`Look up an SCP by it's ID. like "!scp 1048`
  */
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -16,20 +17,28 @@ const {prefix, token} = require("./config/Bot.json")
 const hacker = require('./src/')
 var blacklist = []
 client.on('message', (msg) => {
+	let content = msg.content.toLowerCase()
 	if (msg.author.id != client.user.id && !blacklist.includes(msg.channel.id)) {
-		switch (msg.content) {
-			case prefix+"clear":
-				(msg.guild.me.hasPermission("MANAGE_MESSAGES") ? msg.channel.fetchMessages().then((msgs) => {msg.channel.bulkDelete(msgs)}): msg.reply("I don't have perms for that!"))
-				break;
-			case prefix+"blacklist":
-				(msg.member.hasPermission("ADMINISTRATOR") ? blacklist.push(msg.channel.id):msg.reply("you need admin to do that."))
-				break;
+			switch (content) {
+				case prefix+"clear":
+					(msg.guild.me.hasPermission("MANAGE_MESSAGES") ? msg.channel.fetchMessages().then((msgs) => {msg.channel.bulkDelete(msgs)}): msg.reply("I don't have perms for that!"))
+					break;
+				case prefix+"blacklist":
+					(msg.member.hasPermission("ADMINISTRATOR") ? blacklist.push(msg.channel.id):msg.reply("you need admin to do that."))
+					break;
+			}
+			if (content.includes("r/")) {
+				msg.reply("OwO notices subreddit. Here's a link: "+hacker.reddit(content))
+			}
+			if (content.startsWith(prefix+"scp") && !content.includes("random")) {
+				let x = parseInt(msg.content.replace(/\D+/, ''));
+				(x < 4999 && x > 1 && x % 1 === 0 ? msg.reply("SCP Found: "+hacker.scp(x.toString())):msg.reply("SCP not found."))
+			}
+			if (content.startsWith(prefix+"scp") && content.includes("random")) {
+				msg.reply("SCP: "+hacker.scp((Math.floor(Math.random() * Math.floor(4999))).toString()))
+			}
 		}
-		if (msg.content.includes("r/")) {
-			msg.reply("OwO notices subreddit. Here's a link: "+hacker.reddit(msg.content))
-		}
-	}
-	if (msg.content == prefix+"unblacklist") {
+	if (content == prefix+"unblacklist") {
 		(msg.member.hasPermission("ADMINISTRATOR") ? blacklist.splice(blacklist.indexOf(msg.channel.id), 1) : msg.reply("you need admin for that."))
 	}
 })
--- src/index.js
+++ src/index.js
@@ -12,19 +12,31 @@
  * * * Search for a profile
  * * * Search for an item on a community market
  */
-function parse(mess) {
-	let x = mess.split("r/")[1]
-	let y = (x.includes(" ") ? x.substring(0, x.indexOf(" ")) : x)
-	return y
-}
 module.exports = {
 	/**
 	 * @method reddit()
+	 * @description
+	 * * Returns hyperlink of the provided subreddit in a msg.
 	 * @param msg Any String that includes "r/"
 	 * @BUG Currently only links up to one sub reddit provided.
 	 */
 	reddit:(msg) => {
-		let subreddit = (msg.includes("r/") ? parse(msg) : msg.replace(" ", ""))
+		let subreddit = (msg.includes("r/") ? ((msg) => {
+			let x = msg.split("r/")[1]
+			let y = (x.includes(" ") ? x.substring(0, x.indexOf(" ")) : x)
+			return y
+		})(msg) : msg.replace(" ", ""))
 		return "https://reddit.com/r/"+subreddit
 	},
+	/**
+	 * @method scp()
+	 * @description
+	 * * SCP lookup from 001 to 4999.
+	 * @param scpid Integer String
+	 */
+	scp:(scpid) => {
+		// Every two or one digit SCP's have zeros prefixed
+		let id = (!scpid.startsWith("0") || scpid.length < 3? ("00" + scpid).slice(-3) : scpid)
+		return "http://www.scp-wiki.net/scp-"+id
+	}
 }
