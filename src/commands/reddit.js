5313714a406385e8d66cb3e06012d8c3b6f10a8c
Ubspy <jack-m@live.com>
2019-05-19 09:03:23 -0500

    Fixed some formatting, added comments

--- src/commands/reddit.js
+++ src/commands/reddit.js
@@ -1,6 +1,6 @@
 /**
  * @file reddit.js
- * @author dylhack
+ * @author Ubspy, dylhack
  * @desc
   Whenever 'r/' is detected in a message it will check if a subreddit exists.
   If it doesn't (404, 301, etc.) it won't reply if this happens check the
@@ -9,20 +9,37 @@
  * @param {discord.js <Message>} msg [Message object to work with]
    {@link} https://discord.js.org/#/docs/main/stable/class/Message
  */
+
+ // Getting needed libraries
 const https = require('https');
 const logger = require('../util/logger.js');
 const log = logger("reddit.js", "BLUE");
 
-module.exports = msg => {
-    if (msg.content.includes('r/')) {
+// This returns a function with the passing parameter "msg"
+module.exports = msg =>
+{
+    if (msg.content.includes('r/'))
+    {
+        // Same thing with "let" and "var" here, they're
         let subreddit = msg.content.split("r/")[1].split(" ")[0].toLowerCase();
+
         log("Checking r/" + subreddit);
-        https.get('https://www.reddit.com/r/' + subreddit, res => {
+
+        // Checks to make sure that the subreddit actually exists
+        https.get('https://www.reddit.com/r/' + subreddit, res =>
+        {
+            // Status code 200 means it loaded
             log("status code: " + res.statusCode);
-            if (res.statusCode == 200) {
+            if (res.statusCode == 200)
+            {
                 log("Subreddit hyper-linked: " + subreddit);
                 msg.reply("https://reddit.com/r/" + subreddit);
-            } else log("(" + res.statusCode + ")" + subreddit);
+            }
+            // This runs if there's an error
+            else
+            {
+                log(`(${res.statusCode}) ` + subreddit);
+            }
         })
     }
 };
--- src/util/logger.js
+++ src/util/logger.js
@@ -1,5 +1,8 @@
 var toggle = true; // turn logger on/off.
-var colors = {
+
+// Object that holds specific color values
+var colors =
+{
   "RED": "\x1b[31m",
   "GREEN": "\x1b[32m",
   "YELLOW": "\x1b[33m",
@@ -7,12 +10,28 @@ var colors = {
   "PURPLE": "\x1b[35m",
   "WHITE": "\x1b[37m"
 }
-module.exports = (fileName, color = "WHITE") => {
+
+// When imported, returns a function that needs a fileName and a color
+module.exports = (fileName, color = "WHITE") =>
+{
+  // This could also be "var name = filename" it doesn't make a difference
   let name = fileName
+
+  // This is a string formatter, so it's always in the same format
   let time = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
-  return (head, body) => {
-    if (color == undefined)(body != undefined ? console.log(`[${time}] ` + name + ": " + head, body) : console.log(name + ": " + head))
-    else {
+
+  // Returns a function that has a head and a body part of the message
+  return (head, body) =>
+  {
+    if (color == undefined)
+    {
+      // This console.log is fun, the ${time} returns the formatted time string, and the ticks are ways of appending onto a string
+      // so it's the same as '[' + ${time} + ']' the rest is pretty self explanatory
+      body != undefined ? console.log(`[${time}] ` + name + ": " + head, body) : console.log(`[${time}] ` + name + ": " + head)
+    }
+    else
+    {
+      // This does the same thing, but uses the colors specified before
       body != undefined ? console.log(colors[color.toUpperCase()], `[${time}] ` + head, body) : console.log(colors[color.toUpperCase()], `[${time}] ` + name + ": " + head)
     }
   }
