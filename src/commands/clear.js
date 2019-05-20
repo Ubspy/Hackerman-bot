803b86ebd1b8e6cb1799e5b17f9316ae711e5fd7
Ubspy <jack-m@live.com>
2019-05-20 18:21:57 -0500

    Made clear command remove all messages by calling itself recursively

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -1,26 +1,44 @@
-//const sleep = require('sleep');
-
 // This will clear all the messages in the bot channel
 exports.name = "clear";
+exports.desc = "Clears the bot-commands channel for convenience"
 
 exports.run = (message, logger) => {
     //Gets all messages in the bot-commands channel
-    message.channel.fetchMessages().then(messages =>
-    {
-        //Bulk deletes all the messages
-        message.channel.bulkDelete(messages);
-        logger.info("Command channel has been cleared")
-    }).catch(error => {
+    message.channel.fetchMessages({limit:100}).then(toDelete => {
+        // Calls recursive function
+        // toDelete.array() gets an array of messages, and we get the channel of the first one to pass on to the method
+        deleteMessages(toDelete, message.channel);
+        logger.info("Clearing bot-commands channel");
+    }).catch((error) => {
         // If something goes wrong, log it
-        logger.error(`Failed to clear chat:\n${error}`);
-    });
-
-    //Sends a message
-    message.channel.send("Chat log has been cleared!").then(thisMessage =>
-    {
-        //After the message it sent, it waits three seconds and deletes itself
-        setTimeout(() =>{
-            thisMessage.delete();
-        }, 3000);
+        logger.fatal(`Failed to clear chat:\n${error}`);
     });
 };
+
+deleteMessages = (messages, channel) => {
+    //Bulk deletes all the messages
+    channel.bulkDelete(messages).then(() => {
+        //Gets all messages in the bot-commands channel
+        channel.fetchMessages({limit:100}).then(toDelete => {
+            // If there are messages left, keep deleting
+            if(toDelete.array().length > 0)
+            {
+                // Recursively calls the function to delete the remaining messages
+                deleteMessages(toDelete, channel);
+            }
+            else
+            {
+                //Sends a message saying that it's done clearing
+                channel.send("Chat log has been cleared!").then(thisMessage => {
+                    //After the message it sent, it waits three seconds and deletes itself
+                    setTimeout(() =>{
+                        thisMessage.delete();
+                    }, 3000);
+                });
+            }
+        }).catch((error) => {
+            // If something goes wrong, log it
+            console.log(`Failed to clear chat:\n${error}`);
+        });
+    });
+}
