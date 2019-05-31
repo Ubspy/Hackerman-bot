56a0e71477791c7222b699ae93c5b9960d6fdaa1
Ubspy <jack-m@live.com>
2019-05-31 15:58:55 -0500

    Added base for steam command

---
+++ src/commands/steam.js
@@ -0,0 +1,8 @@
+// This will handle the commands for our steam subsystem, current functionality is a steam wishlist that will @ everyone if there's a wanted game on sale
+exports.name = "steam";
+exports.desc = "The steam subsystem of the bot, for more help type '!steam help'";
+exports.args = ['action'];
+
+exports.run = (message, args, logger) => {
+    
+};
