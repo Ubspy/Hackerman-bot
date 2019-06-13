0e1de2c577daebaf9ff1bae55e39d4ef8543ef5c
Ubspy <jack-m@live.com>
2019-06-13 16:04:51 -0500

    Added currency formatting for the game prices

--- src/utils/sale-notifier.js
+++ src/utils/sale-notifier.js
@@ -7,6 +7,11 @@ const SteamAPI = require('steamapi');
 const steam = new SteamAPI(config.steamToken);
 
 module.exports = (announcementChannel, logger) => {
+    var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+
     // Job that'll run everyday at 6 pm on machine's localtime
     var job = schedule.scheduleJob("* 18 * * *", () => {
         wishlist.games.forEach(game => {
@@ -15,7 +20,7 @@ module.exports = (announcementChannel, logger) => {
                 if(details.price_overview.discount_percent > 0 && !game.onSale)
                 {
                     // Announces it in the announcement channel
-                    announcementChannel.send(`${game.name} is ${details.price_overview.discount_percent}% off!. It's on sale from ${details.price_overview.initial} to ${details.price_overview.final}\n${game.link}`);
+                    announcementChannel.send(`${game.name} is ${details.price_overview.discount_percent}% off!. It's on sale from ${formatter.format(details.price_overview.initial/100)} to ${formatter.format(details.price_overview.final/100)}\n${game.link}`);
 
                     // Changes game's sale state to true and writes to the file
                     game.onSale = true;
