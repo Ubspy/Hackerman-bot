cb901d65eb077fd2156405660fbb6c95b783b863
Ubspy <jack-m@live.com>
2019-06-17 12:39:38 -0500

    Fixed the game names being really stupid, fixed some reply messages, emptied wishlist json file

--- src/commands/steam/steam-add.js
+++ src/commands/steam/steam-add.js
@@ -14,14 +14,14 @@ exports.args = ["game name"];
 // This has to be an 
 exports.run = async (message, args, logger) => {
     // Since this only takes one argument, and the name of some games have spaces, we're gonna join all the elements together from the args array
-    var gameName = args.join(' ');
+    var gameToSearch = args.join(' ');
 
     // cx: custom engine key
     // q: search query
     // auth: api key
     const options = {
         cx: config.googleSearchEngineID,
-        q: `${gameName} -Franchise`, // This bit removes the franchise pages from steam, because if you search like "fallout" it gives you the fallout franchise page
+        q: `${gameToSearch} -Franchise`, // This bit removes the franchise pages from steam, because if you search like "fallout" it gives you the fallout franchise page
         auth: config.googleToken
     };
 
@@ -33,9 +33,6 @@ exports.run = async (message, args, logger) => {
     // Gets the link from the google search results
     var link = res.data.items[0].link;
 
-    // Gets the name from the result title
-    var gameName = res.data.items[0].title;
-
     // Gets the steam id by splitting it with / and takes the last element of the steam url, which is the id
     // The epic thing is every single steam url is different so you have do some oddball shit just to get the ID
     // The app ID is (hopefully) always after 'app/' so we look for that and get the ID afterwards
@@ -46,22 +43,29 @@ exports.run = async (message, args, logger) => {
     // Checks to see if the wishlist already contains the game
     if(!wishlist.games.some(game => game.id === gameID))
     {
-        // Notifies the user that the game was added
-        message.reply(`Added ${gameName} to wishlish\n${link}`);
-
         // Gets the game details from steam
         steam.getGameDetails(gameID).then(details => {
+            // Gets the name from steam data
+            var gameName = details.name;
+
             var gameOnSale = (details.price_overview.discount_percent > 0);
 
             // Adds game to wishlist object, then writes it to the file
             wishlist.games.push({"name" : gameName, "id" : gameID, "link" : link, "onSale" : gameOnSale});
             fs.writeFileSync(`${__dirname}/../../../config/wishlist.json`, JSON.stringify(wishlist));
+
+            // Notifies the user that the game was added
+            message.reply(`Added ${gameName} to wishlish\n${link}`);
+            logger.info(`Added game ${gameName} with id ${gameID} from wishlist`);
+
+        }).catch(error => {
+            logger.error(`Failed to add game ${gameToSearch} to wishlist`);
         });
-        
-        logger.info(`Removed game ${gameName} with id ${gameID} from wishlist`);
     }
     else
     {
-        message.reply(`${gameName} is already on the wishlist\n${link}`);
+        steam.getGameDetails(gameID).then(details => {
+            message.reply(`${details.name} is already on the wishlist\n${link}`);
+        });
     }
 };
--- src/commands/steam/steam-remove.js
+++ src/commands/steam/steam-remove.js
@@ -4,6 +4,9 @@ const search = google.customsearch('v1');
 const config = require('../../../config/config.json'); // Goes 3 folders back to get config file
 const wishlist = require('../../../config/wishlist.json');
 
+const SteamAPI = require('steamapi');
+const steam = new SteamAPI(config.steamToken);
+
 exports.name = "remove";
 exports.desc = "Removes a steam game to the wishlist of games";
 exports.args = ["game name"];
@@ -11,14 +14,14 @@ exports.args = ["game name"];
 // This has to be an 
 exports.run = async (message, args, logger) => {
     // Since this only takes one argument, and the name of some games have spaces, we're gonna join all the elements together from the args array
-    var gameName = args.join(' ');
+    var gameToSearch = args.join(' ');
 
     // cx: custom engine key
     // q: search query
     // auth: api key
     const options = {
         cx: config.googleSearchEngineID,
-        q: `${gameName} -Franchise`, // This bit removes the franchise pages from steam, because if you search like "fallout" it gives you the fallout franchise page
+        q: `${gameToSearch} -Franchise`, // This bit removes the franchise pages from steam, because if you search like "fallout" it gives you the fallout franchise page
         auth: config.googleToken
     };
 
@@ -30,9 +33,6 @@ exports.run = async (message, args, logger) => {
     // Gets the link from the google search results
     var link = res.data.items[0].link;
 
-    // Gets the name from the result title
-    var gameName = res.data.items[0].title;
-
     // Gets the steam id by splitting it with / and takes the last element of the steam url, which is the id
     // The epic thing is every single steam url is different so you have do some oddball shit just to get the ID
     // The app ID is (hopefully) always after 'app/' so we look for that and get the ID afterwards
@@ -41,17 +41,27 @@ exports.run = async (message, args, logger) => {
     // Checks to see if the wishlist already contains the game
     if(wishlist.games.some(game => game.id === gameID))
     {
-        // Notifies the user that the game was added
-        message.reply(`Removed ${gameName} to wishlish\n${link}`);
-        
-        // Removes game from wishlist object, then writes it to the file
-        wishlist.games.pop({"name" : gameName, "id" : gameID});
-        fs.writeFileSync(`${__dirname}/../../../config/wishlist.json`, JSON.stringify(wishlist));
+        // Gets the game details from steam
+        steam.getGameDetails(gameID).then(details => {
+            // Gets the name from steam data
+            var gameName = details.name;
 
-        logger.info(`Added game ${gameName} with id ${gameID} to wishlist`);
+            // Removes game from wishlist object, then writes it to the file
+            wishlist.games.pop({"name" : gameName, "id" : gameID});
+            fs.writeFileSync(`${__dirname}/../../../config/wishlist.json`, JSON.stringify(wishlist));
+
+            // Notifies the user that the game was added
+            message.reply(`Removed ${gameName} from wishlish\n${link}`);
+            logger.info(`Removed game ${gameName} with id ${gameID} from wishlist`);
+
+        }).catch(error => {
+            logger.error(`Failed to add game ${gameToSearch} to wishlist`);
+        });
     }
     else
     {
-        message.reply(`${gameName} is not on the wishlist\n${link}`);
+        steam.getGameDetails(gameID).then(details => {
+            message.reply(`${details.name} is not on the wishlist\n${link}`);
+        });
     }
 };
