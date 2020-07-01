3642ee61de58e46c39f0e4769507737342b499aa
Ubspy <jack-m@live.com>
2020-07-01 16:05:49 -0500

    Ok so the bulk deleting is still fucked, so I made it recursive again

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -6,27 +6,80 @@ exports.args = [];
 exports.run = (message, args, logger) => {
     logger.info("Clearing bot-commands channel");
 
-    // We first fetch all the messages in the channel
-    message.channel.messages.fetch().then(messages => {
-        // We then bulk delete all those messages, this only works for messages less than 2 weeks old
-        // The true argument makes it so messages older than 2 weeks stay and it doesn't cause a giant crash
-        message.channel.bulkDelete(messages, true).then(messages => {
-            // Incase there are any messages left that were older than 2 weeks, we delete them the lame way
-            if(messages.array.length > 0)
-            {
-                // Loop through all of them and delete
-                for(var i = 0; i < messages.array.length; i++)
-                {
-                    messages.array[i].delete();
-                }
-            }
+    
+    // We first get all the messages in the channel from the cache
+    messages = message.channel.messages.cache;
 
+    message.channel.bulkDelete(messages, true).then(deleted => {
+
+        // We then check for messages left
+        message.channel.messages.fetch({limit: 100}).then(messages => {
+            if(messages.size > 0 && deleted.size > 0) // We check for messages left size and deleted messages size incase the ones left can't be bulk deleted
+            {
+                fetchDelete(messages, message.channel, logger);
+            }
+            else if(messages.size > 0 && deleted.size == 0)
+            {
+                // Incase there are any messages left that were older than 2 weeks, we delete them the lame way
+                forDelete(message.channel, logger);
+            }
+            else
+            {
+                // Send a temp message saying it's done
+                message.channel.send(`Chat log has been cleared!`).then(message => {
+                    setTimeout(() => {
+                        message.delete();
+                    }, 3000);
+                });
+            }
+        });
+    });
+};
+
+fetchDelete = (message, channel, logger) => {
+    channel.bulkDelete(messages, true).then(deleted => {
+        // Same procedure as before
+        channel.messages.fetch({limit: 100}).then(messages => {
+            if(messages.size > 0 && deleted.size > 0)
+            {
+                fetchDelete(messages, channel, logger);
+            }
+            else if(messages.size > 0 && deleted.size == 0)
+            {
+                forDelete(messages, channel, logger);
+            }
+            else
+            {
+                // Send a temp message saying it's done
+                channel.send(`Chat log has been cleared!`).then(message => {
+                    setTimeout(() => {
+                        message.delete();
+                    }, 3000);
+                });
+            }
+        });
+    });
+}
+
+forDelete = (messages, channel, logger) => {
+    for(var i = 0; i < messages.size; i++)
+    {
+        messages.array[i].delete();
+    }
+
+    channel.messages.fetch({limit: 100}).then(messages => {
+        if(messages.size > 0 && deleted.size > 0)
+        {
+            forDelete(messages, channel, logger);
+        }
+        else
+        {
             // Send a temp message saying it's done
-            message.channel.send(`Chat log has been cleared!`).then(message => {
+            channel.send(`Chat log has been cleared!`).then(message => {
                 setTimeout(() => {
                     message.delete();
                 }, 3000);
             });
-        });
+        }
     });
-};
+}
